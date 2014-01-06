dd4e24020487a70df1e340f498e423261a6d1c1c
Zulfat <zulfat@138.1.168.192.in-addr.arpa>
2014-01-06 18:03:12 +0400

    as in ibilio

--- drawing/drawingClass.m
+++ drawing/drawingClass.m
@@ -36,17 +36,24 @@
     CGContextSetLineCap(context, kCGLineCapRound);
     for (int i=0; i<[obj.drawingObjects count];i++) {
         NSArray* pointsArray = [obj.drawingObjects objectAtIndex:i];
-        Aix=([[pointsArray objectAtIndex:2] CGPointValue].x-[[pointsArray objectAtIndex:0] CGPointValue].x)/4;
-        Aiy=([[pointsArray objectAtIndex:2] CGPointValue].y-[[pointsArray objectAtIndex:0] CGPointValue].y)/4;
-        dx=1;
-        dy=1;
-        Bi=-0.25;
+        dx=([[pointsArray objectAtIndex:1] CGPointValue].x-[[pointsArray objectAtIndex:0] CGPointValue].x)/3;
+        dy=([[pointsArray objectAtIndex:1] CGPointValue].y-[[pointsArray objectAtIndex:0] CGPointValue].y)/3;;
+        Aix=([[pointsArray objectAtIndex:2] CGPointValue].x-[[pointsArray objectAtIndex:0] CGPointValue].x-dx)/4;
+        Aiy=([[pointsArray objectAtIndex:2] CGPointValue].y-[[pointsArray objectAtIndex:0] CGPointValue].y-dy)/4;
+        Bi=1;
         for (int j=0;j<[pointsArray count]-2;j++) {
             startPoint = [[pointsArray objectAtIndex:j] CGPointValue];
             endPoint = [[pointsArray objectAtIndex:j+1] CGPointValue];
             nextPoint = [[pointsArray objectAtIndex:j+2] CGPointValue];
-            bezierHelper1 = CGPointMake(endPoint.x + (endPoint.x-nextPoint.x)/8 + (startPoint.x-endPoint.x)/4, endPoint.y + (endPoint.y-nextPoint.y)/8 + (startPoint.y-endPoint.y)/5);
-            bezierHelper2 = CGPointMake(endPoint.x + (endPoint.x-nextPoint.x)/4 + (startPoint.x-endPoint.x)/8, endPoint.y + (endPoint.y-nextPoint.y)/4 + (startPoint.y-endPoint.y)/8);
+            bezierHelper1 = CGPointMake(startPoint.x + dx, startPoint.y+dy);
+            //bezierHelper1 = CGPointMake(endPoint.x + (endPoint.x-nextPoint.x)/8 + (startPoint.x-endPoint.x)/4, endPoint.y + (endPoint.y-nextPoint.y)/8 + (startPoint.y-endPoint.y)/5);
+            //bezierHelper2 = CGPointMake(endPoint.x + (endPoint.x-nextPoint.x)/4 + (startPoint.x-endPoint.x)/8, endPoint.y + (endPoint.y-nextPoint.y)/4 + (startPoint.y-endPoint.y)/8);
+            Aix = (nextPoint.x - startPoint.x - Aix)/(4+Bi);
+            Aiy = (nextPoint.y - startPoint.y - Aiy)/(4+Bi);
+            Bi=-1/(4+Bi);
+            dx=Aix+Bi*dx;
+            dy=Aiy+Bi*dy;
+            bezierHelper2 = CGPointMake(endPoint.x-dx, endPoint.y-dy);
             CGContextSetRGBStrokeColor(context, 255, 0, 255, 1);
             CGContextBeginPath(context);
             CGContextMoveToPoint(context, startPoint.x, startPoint.y);
